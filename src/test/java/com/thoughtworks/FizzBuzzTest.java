04321963207c1f249d00a28de1b21eff95a74bf3
Geng Cai <geng.cai@thoughtworks.com>
2020-01-10 20:52:12 +0800

    [Geng]refactor: import static method and variable form FizzBuzz

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -3,13 +3,17 @@ package com.thoughtworks;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static com.thoughtworks.FizzBuzz.fizzBuzz;
+import static com.thoughtworks.FizzBuzz.FIZZ;
+import static com.thoughtworks.FizzBuzz.BUZZ;
+import static com.thoughtworks.FizzBuzz.WHIZZ;
 
 public class FizzBuzzTest {
 
     @Test
-    public void basic_fizz_3_buzz_5_whizz_7_test() {
-        Assert.assertEquals(FizzBuzz.FIZZ, FizzBuzz.fizzBuzz(3));
-        Assert.assertEquals(FizzBuzz.BUZZ, FizzBuzz.fizzBuzz(5));
-        Assert.assertEquals(FizzBuzz.WHIZZ, FizzBuzz.fizzBuzz(7));
+    public void basic_3_5_7_test() {
+        Assert.assertEquals(FIZZ, fizzBuzz(3));
+        Assert.assertEquals(BUZZ, fizzBuzz(5));
+        Assert.assertEquals(WHIZZ, fizzBuzz(7));
     }
 }
